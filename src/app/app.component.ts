e28f1d8500a147e93329fdcbaa3fcbbd4e200702
romeovasil <romeo.vasil@icloud.com>
2023-05-20 16:01:58 +0300

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Student} from "./student";
+import {StudentService} from "./student.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'frontend';
+  students :Map<string,string>;
+  currentDate: number;
+  constructor(private studentService:StudentService) {
+    this.students=new Map<string, string>();
+    this.currentDate=new Date().getDate();
+  }
+
+  ngOnInit(): void {
+    let students:Student[]=[];
+    this.studentService.getStudents().subscribe(
+      data=>{
+        students=data;
+        for(const student of students){
+          if(this.currentDate%this.getVowels(student.name)==0)
+            this.students.set(student.name,"good day")
+          else
+            this.students.set(student.name,"bad day")
+        }
+      }
+    )
+
+  }
+  getVowels(str:string) {
+    var m = str.match(/[aeiou]/gi);
+    return m === null ? 0 : m.length;
+  }
+
+  protected readonly name = name;
 }
